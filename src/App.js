8146f3da1fd147b538b0e238a684dea1f8a80db5
deepforce08 <canerozus@hotmail.com>
2021-08-31 14:58:38 +0300

    feature: 'add to basket' added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import { connect } from 'react-redux';
+import { Route } from "react-router-dom";
 
-function App() {
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+
+
+ const App = props => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Shopping List with React and Redux 
+      </h1>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
     </div>
   );
 }
-
-export default App;
+const mapStatetoProps = state => { 
+  return {
+    bookList: state.bookList
+  }
+}
+export default connect(mapStatetoProps)(App);
